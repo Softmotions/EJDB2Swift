bf9ea394a4a42c7442ef93c17036429413f80ae7
Anton Adamansky <adamansky@gmail.com>
2021-03-21 22:16:44 +0700

    * Updated to ejdb2 2.0.59

--- Sources/EJDB2/EJDB2.swift
+++ Sources/EJDB2/EJDB2.swift
@@ -407,7 +407,8 @@ public final class EJDB2Builder {
         access_token_len: cHttpAccessToken.count,
         blocking: false,
         read_anon: httpReadAnon ?? false,
-        max_body_size: Int(httpMaxBodySize ?? 0)
+        max_body_size: Int(httpMaxBodySize ?? 0),
+        cors: false
       ),
       no_wal: walDisabled ?? false,
       sort_buffer_sz: sortBufferSize ?? 0,
